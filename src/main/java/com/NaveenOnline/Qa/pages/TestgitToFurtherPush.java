f418e81b762d5eaaff9489acf00ea41ebc735613
The MAN <apple@THEMan.local>
2024-10-22 11:04:44 +0530

    Testing modification

--- src/main/java/com/NaveenOnline/Qa/pages/TestgitToFurtherPush.java
+++ src/main/java/com/NaveenOnline/Qa/pages/TestgitToFurtherPush.java
@@ -5,5 +5,7 @@ public class TestgitToFurtherPush {
     public static void TestgitToFurtherPush(){
 
         System.out.println("Testing Futher Push");
+
+        System.out.println("Testing once again");
     }
 }
